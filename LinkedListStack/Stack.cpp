462cecfa7532d97e5f30ecf9d1526c000b28f5a3
igolino3@hotmail.com <igorsmarques0@gmail.com>
2019-05-17 21:15:34 -0300

    All function working, missing implementation of special functions for copy and assign operator!

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,65 @@
 #include "Stack.h"
+
+void Stack::Push(int val)
+{
+	if (Empty()) {
+		pBegin = new Element(val);
+	}
+	else {
+		Element* pLast = pBegin;
+		while (pLast->pNext != nullptr) {
+			pLast = pLast->pNext;
+		}
+		pLast->pNext = new Element(val);
+	}
+}
+
+int Stack::Pop()
+{
+	if (Empty()) {
+		return -1;
+	}
+	else {
+		Element* pLast = pBegin;
+		Element* pBeforeLast = pBegin;
+		while (pLast->pNext != nullptr) {
+			pBeforeLast = pLast;
+			pLast = pLast->pNext;
+		}
+		int data = pLast->data;
+		if (pLast != pBeforeLast) {
+			delete pLast;
+			pBeforeLast->pNext = nullptr;
+		}
+		else {
+			delete pBegin;
+			pBegin = nullptr;
+		}
+		return data;
+	}
+}
+
+int Stack::Size() const
+{
+	if (Empty()) {
+		return 0;
+	}
+	else {
+		int counter = 1;
+		Element* pLast = pBegin;
+		while (pLast->pNext != nullptr) {
+			counter++;
+			pLast = pLast->pNext;
+
+		}
+		return counter;
+	}
+}
+
+bool Stack::Empty() const
+{
+	if (pBegin == nullptr) {
+		return true;
+	}
+	return false;
+}
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -2,9 +2,28 @@
 
 class Stack
 {
+private:
+	class Element {
+	public:
+		Element() = default;
+		Element(const int val) {
+			data = val;
+		}
+		~Element() {
+			delete pNext;
+		}
+
+	public:
+		int data;
+		Element* pNext = nullptr;
+	};
+
 public:
 	void Push( int val );
 	int Pop();
 	int Size() const;
 	bool Empty() const;
+
+private:
+	Element* pBegin = nullptr;
 };
